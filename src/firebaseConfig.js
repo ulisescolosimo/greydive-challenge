e270d83ff1652bb55f935d00279107ab6cd93f58
Ulises Colosimo <colosimo101@gmail.com>
2023-01-09 18:11:30 -0300

    Modified .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.RVITE_MESSAGING_SENDERIN_ID;
+const appId = import.meta.env.VITE_APP_ID;
+const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCc4JbjLLt9_-l4rC4KXusCaPklkPgLXA8",
-  authDomain: "greydive-e3803.firebaseapp.com",
-  projectId: "greydive-e3803",
-  storageBucket: "greydive-e3803.appspot.com",
-  messagingSenderId: "227063545466",
-  appId: "1:227063545466:web:d501c34f572764b4df05c3",
-  measurementId: "G-T0GB1ND76C",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
